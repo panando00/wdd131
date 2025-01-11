064c882d6530b609c65a9aebd35b6272c32aa8b1
panando00 <prosperzvidzai00@gmail.com>
2025-01-11 22:28:22 +0200

    initial commit

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Get the current year for the copyright
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Get the last modified date
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
+
