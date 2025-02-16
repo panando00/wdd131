a2c748720339dbe56e4685f0d23213f1413a9bc2
panando00 <prosperzvidzai00@gmail.com>
2025-02-16 21:45:50 +0200

    week06

---
+++ scripts/doctor.js
@@ -0,0 +1,34 @@
+// Book data
+const featuredBooks = [
+    {
+        title: "Introduction to Computer Science",
+        author: "Prosper Zvidzai",
+        price: 79.99,
+        originalPrice: 129.99,
+        image: "placeholder-book.jpg"
+    },
+    {
+        title: "Advanced Mathematics",
+        author: "Prosper Zvidzai",
+        price: 89.99,
+        originalPrice: 149.99,
+        image: "placeholder-book.jpg"
+    },
+    {
+        title: "Business Economics",
+        author: "Prosper Zvidzai",
+        price: 69.99,
+        originalPrice: 119.99,
+        image: "placeholder-book.jpg"
+    },
+    {
+        title: "Modern Physics",
+        author: "Prosper Zvidzai",
+        price: 94.99,
+        originalPrice: 159.99,
+        image: "placeholder-book.jpg"
+    }
+];
+
+// Mobile menu toggle
+document.querySelector('.menu-toggle').addEventListener('click',
